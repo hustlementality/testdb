78ec5c446691fae9e998ef4876f11df86486f168
hustlementality <hustle.mentality09@gmail.com>
2025-01-19 18:22:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
 const form = document.getElementById('email-form');
 const emailInput = document.getElementById('email');
 
@@ -5,7 +6,7 @@ const emailInput = document.getElementById('email');
 const SUPABASE_URL = 'https://bddwcstwuliivbhwueni.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkZHdjc3R3dWxpaXZiaHd1ZW5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTIwMzIsImV4cCI6MjA1Mjg4ODAzMn0.CbEtR82Boqt0yhIkH6iOLVOOkjPRfvESYQon64NsMsY';
 
-const { createClient } = supabase;
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 form.addEventListener('submit', async (event) => {
@@ -13,8 +14,9 @@ form.addEventListener('submit', async (event) => {
     const email = emailInput.value;
 
     if (!email) return;
+    
 
-    const { data, error } = await supabase
+    const { error } = await supabase
         .from('emails')
         .insert([{ email }]);
 
